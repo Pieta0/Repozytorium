db8a4116d5d7d468b5f839fe45784e699cc18d37
Pieta <piotr.szczesniak01@gmail.com>
2022-08-12 11:05:52 +0200

    asd

---
+++ src/Zmienne.java
@@ -0,0 +1,19 @@
+public class Zmienne {
+
+
+
+
+    public static void main(String[] args) {
+
+
+        String name= "Piotrek";
+        int liczba = 1;
+        int liczba2 = 2;
+        int result = liczba+liczba2;
+
+        name = "Kasia";
+
+        System.out.println(name);
+        System.out.println(result);
+    }
+}
