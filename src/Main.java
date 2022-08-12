f721094d2977caed54e26972c8b267ae96eb8428
Pieta <piotr.szczesniak01@gmail.com>
2022-08-12 11:06:22 +0200

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
 
+    int asd = 123;
+    Integer aha = 412412;
+
+
         public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("sdgfsdag");
+
 
 
     }
---
+++ src/OperatoryMatematyczne.java
@@ -0,0 +1,22 @@
+public class OperatoryMatematyczne {
+
+    public static void main(String[] args) {
+        int firstNumber = 4;
+        int secondNumber = 6;
+        float thirdNumber = 4.0F;
+
+
+        int addition = firstNumber + secondNumber;
+        int substraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        float division = thirdNumber/secondNumber;
+        int mod = secondNumber%firstNumber;
+
+
+        System.out.println(addition);
+        System.out.println(substraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(mod);
+    }
+}
